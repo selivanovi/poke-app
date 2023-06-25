8d7cc5d9d0988eb0ef2f1628557ffe09e988821e
Illia Selivanov <ilia.selivanon@nitrix.io>
2023-06-25 20:24:30 +0300

    add view binding in MainActivity

--- app/src/main/java/com/example/pokeapp/MainActivity.kt
+++ app/src/main/java/com/example/pokeapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.pokeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pokeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _mainActivityBinding: ActivityMainBinding? = null
+    private val mainActivityBinding: ActivityMainBinding
+        get() = _mainActivityBinding ?: throw NullPointerException()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainActivityBinding.root)
     }
 }
